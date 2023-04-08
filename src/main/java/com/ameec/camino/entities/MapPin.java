cabd1b4b4e33e2089a37f12cb2d788e1e89a9359
Amee <ameecova@yahoo.com>
2023-04-08 21:47:38 +0200

    Create tables

--- src/main/java/com/ameec/camino/entities/MapPin.java
+++ src/main/java/com/ameec/camino/entities/MapPin.java
@@ -1,5 +1,10 @@
 package com.ameec.camino.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="Map_pins")
 public class MapPin {
     
 }
--- src/main/java/com/ameec/camino/entities/User.java
+++ src/main/java/com/ameec/camino/entities/User.java
@@ -1,5 +1,10 @@
 package com.ameec.camino.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="Users")
 public class User {
     
 }
