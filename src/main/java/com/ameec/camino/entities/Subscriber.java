ce510f03d4cc299eaf73d1450f58c4d85eab2385
Amee <ameecova@yahoo.com>
2023-04-09 17:12:18 +0200

    Remove unused imports

--- src/main/java/com/ameec/camino/entities/Subscriber.java
+++ src/main/java/com/ameec/camino/entities/Subscriber.java
@@ -1,13 +1,10 @@
 package com.ameec.camino.entities;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
