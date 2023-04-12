34a249daaa8b322cc6d414f40129a13205466e5a
Amee <ameecova@yahoo.com>
2023-04-12 18:36:44 +0200

    Autowire password encoder bean to UserServiceImpl

--- src/main/java/com/ameec/camino/services/UserServiceImpl.java
+++ src/main/java/com/ameec/camino/services/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package com.ameec.camino.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.ameec.camino.repositories.UserRepository;
@@ -9,4 +10,7 @@ import com.ameec.camino.repositories.UserRepository;
 public class UserServiceImpl {
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 }
