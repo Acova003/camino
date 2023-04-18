3c6ff3eda523286e03898d0b7eec7b81e88ce92d
Amee <ameecova@yahoo.com>
2023-04-18 20:18:48 +0200

    Add find by email definition to user repository

--- src/main/java/com/ameec/camino/repositories/UserRepository.java
+++ src/main/java/com/ameec/camino/repositories/UserRepository.java
@@ -1,10 +1,17 @@
 package com.ameec.camino.repositories;
 
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.ameec.camino.entities.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
+
+    Optional<User> findByEmail(String subscriberEmail) {
+        return null;
+    }
     
 }
