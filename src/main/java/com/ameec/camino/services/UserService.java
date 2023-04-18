680304390b5df1bdb72e4a0a77426c35e1c2da4d
Amee <ameecova@yahoo.com>
2023-04-18 21:01:57 +0200

    Create subscribe and end subscription methods

--- src/main/java/com/ameec/camino/services/UserService.java
+++ src/main/java/com/ameec/camino/services/UserService.java
@@ -1,15 +1,17 @@
 package com.ameec.camino.services;
 
 import java.util.List;
-
-import org.springframework.transaction.annotation.Transactional;
+import java.util.Optional;
 
 import com.ameec.camino.dtos.UserDto;
+import com.ameec.camino.entities.User;
 
 public interface UserService {
 
     List<String> addUser(UserDto userDto);
 
     List<String> userLogin(UserDto userDto);
+
+    Optional<User> findByEmail(String email);
 
 }
