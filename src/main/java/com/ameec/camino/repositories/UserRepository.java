60d6061a489df2924f91b0735b1ee5ca4a5f3910
Amee <ameecova@yahoo.com>
2023-04-18 21:10:38 +0200

    Remove addUser and userLogin

--- src/main/java/com/ameec/camino/repositories/UserRepository.java
+++ src/main/java/com/ameec/camino/repositories/UserRepository.java
@@ -1,22 +1,12 @@
 package com.ameec.camino.repositories;
 
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ameec.camino.dtos.UserDto;
 import com.ameec.camino.entities.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
-
-    List<String> addUser(UserDto userDto);
-
-    List<String> userLogin(UserDto userDto);
     
     Optional<User> findByEmail(String email);
-    
 }
