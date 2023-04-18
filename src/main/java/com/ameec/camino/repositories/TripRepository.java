850b6ffe9868362ded35697021de33e701e054e1
Amee <ameecova@yahoo.com>
2023-04-18 22:27:34 +0200

    Add methods to interface

--- src/main/java/com/ameec/camino/repositories/TripRepository.java
+++ src/main/java/com/ameec/camino/repositories/TripRepository.java
@@ -1,10 +1,17 @@
 package com.ameec.camino.repositories;
 
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.ameec.camino.entities.Trip;
 
 @Repository
 public interface TripRepository extends JpaRepository<Trip, Long> {
+
+    Optional<Trip> findByUserId(Long userId);
+
+    void deleteByUserId(Long userId);
     
 }
