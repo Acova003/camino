18560edb42c5086567104cac29b64cb163ad3a06
Amee <ameecova@yahoo.com>
2023-04-21 16:54:20 +0200

    Remove display name

--- src/main/java/com/ameec/camino/dtos/SubscriberDto.java
+++ src/main/java/com/ameec/camino/dtos/SubscriberDto.java
@@ -17,15 +17,12 @@ public class SubscriberDto {
     private String display_name;
     private User subscribedUser;
 
-    public SubscriberDto(Subscriber subscriber){
+    public SubscriberDto(SubscriberDto subscriber){
         if (subscriber.getId() != null) {
             this.id = subscriber.getId();
         }
         if (subscriber.getTrip() != null) {
             this.trip = subscriber.getTrip();
-        }
-        if (subscriber.getDisplay_name() != null) {
-            this.display_name = subscriber.getDisplay_name();
         }
         if (subscriber.getSubscribedUser() != null) {
             this.subscribedUser = subscriber.getSubscribedUser();
