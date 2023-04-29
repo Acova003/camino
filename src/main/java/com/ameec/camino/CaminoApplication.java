55d546d1d73ad19621d5102839260c13120c3899
Amee <ameecova@yahoo.com>
2023-04-29 15:11:13 +0200

    Load env variables in main

--- src/main/java/com/ameec/camino/CaminoApplication.java
+++ src/main/java/com/ameec/camino/CaminoApplication.java
@@ -2,11 +2,13 @@ package com.ameec.camino;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class CaminoApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
 		SpringApplication.run(CaminoApplication.class, args);
 	}
 
