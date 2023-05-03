dbe6155a075186c6ea0c4556cec9d2341b318d3f
Amee <ameecova@yahoo.com>
2023-05-03 18:45:14 +0200

    Fix env variables for railway and remove env main

--- src/main/java/com/ameec/camino/CaminoApplication.java
+++ src/main/java/com/ameec/camino/CaminoApplication.java
@@ -8,18 +8,19 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class CaminoApplication {
 
 	public static void main(String[] args) {
-		// Dotenv dotenv = Dotenv.configure().directory("src/main/resources").load();
+        // find railway specific way to point to env variables
+		Dotenv dotenv = Dotenv.configure().directory("src/main/resources").load();
 
 
 
 
-		// String dbUrl = dotenv.get("DB_URL");
-        // String dbUser = dotenv.get("DB_USERNAME");
-        // String dbPassword = dotenv.get("DB_PASSWORD");
+		String dbUrl = dotenv.get("DB_URL");
+        String dbUser = dotenv.get("DB_USERNAME");
+        String dbPassword = dotenv.get("DB_PASSWORD");
 
-        // System.setProperty("spring.datasource.url", dbUrl);
-        // System.setProperty("spring.datasource.username", dbUser);
-        // System.setProperty("spring.datasource.password", dbPassword);
+        System.setProperty("spring.datasource.url", dbUrl);
+        System.setProperty("spring.datasource.username", dbUser);
+        System.setProperty("spring.datasource.password", dbPassword);
 		SpringApplication.run(CaminoApplication.class, args);
 	}
 
