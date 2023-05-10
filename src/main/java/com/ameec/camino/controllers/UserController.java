8ec9a08f12cc65149bb61054d71e3d0fcada4372
Amee <ameecova@yahoo.com>
2023-05-10 18:24:24 +0200

    Create user endpoints

---
+++ src/main/java/com/ameec/camino/controllers/UserController.java
@@ -0,0 +1,35 @@
+package com.ameec.camino.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ameec.camino.dtos.UserDto;
+import com.ameec.camino.services.UserService;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @PostMapping("/register")
+    public List<String> addUser(@RequestBody UserDto userDto){
+        String passHash = passwordEncoder.encode(userDto.getPassword());
+        userDto.setPassword(passHash);
+        return userService.addUser(userDto);
+    }
+
+    @PostMapping("/login")
+    public List<String> userLogin(@RequestBody UserDto userDto){
+        return userService.userLogin(userDto);
+    }
+}
+
