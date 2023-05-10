7e6e3366a0ed40e80d630496c5820df6883efcb1
Amee <ameecova@yahoo.com>
2023-05-10 18:23:40 +0200

    Remove comments

--- src/main/java/com/ameec/camino/controllers/HomeController.java
+++ src/main/java/com/ameec/camino/controllers/HomeController.java
@@ -1,14 +1,15 @@
 package com.ameec.camino.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
-    // show home method
-    @RequestMapping("")
-    public String home() {
-        return "home";
-    }
+    
+     @GetMapping("")
+     public String home() {
+         return "home";
+     }
 }
