6c445a4f3e3f8a56a848fcb03837b6e2507a8184
Amee <ameecova@yahoo.com>
2023-05-25 00:19:00 +0200

    change from find by email to subscribe

--- src/main/java/com/ameec/camino/services/UserService.java
+++ src/main/java/com/ameec/camino/services/UserService.java
@@ -1,17 +1,14 @@
 package com.ameec.camino.services;
 
 import java.util.List;
-import java.util.Optional;
-
 import com.ameec.camino.dtos.UserDto;
-import com.ameec.camino.entities.User;
 
-public interface UserService {
+public interface UserService{
 
     List<String> addUser(UserDto userDto);
 
-    List<String> userLogin(UserDto userDto);
+    List<String> userLogin(UserDto userDto, String password);
 
-    Optional<User> findByEmail(String email);
+    List<String> subscribe(String email, Long subscriberId);
 
 }
