5ff6cf40c8c4d962ac49e22a74a9cb65c74759bc
Amee <ameecova@yahoo.com>
2023-06-04 18:52:52 +0200

    Comment out env enpoint for dev

--- src/main/java/com/ameec/camino/controllers/EnvController.java
+++ src/main/java/com/ameec/camino/controllers/EnvController.java
@@ -1,12 +1,13 @@
 package com.ameec.camino.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
+// import org.springframework.web.bind.annotation.GetMapping;
 
-@
+@Controller
 public class EnvController {
-    @GetMapping("/api/env")
-    public String getEnv() {
-        String apiKey = System.getenv(GOOGLE_MAPS_API_KEY);
-        return apiKey;
-    }
+    // @GetMapping("/api/env")
+    // public String getEnv() {
+    //     String apiKey = System.getenv(GOOGLE_MAPS_API_KEY);
+    //     return apiKey;
+    // }
 }
