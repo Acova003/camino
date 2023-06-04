e976d3a170b09f18ebc13e97d632c325e4b36a65
Amee <ameecova@yahoo.com>
2023-06-04 18:46:20 +0200

    Add locations enpoint and remove unused imports

--- src/main/java/com/ameec/camino/controllers/TripController.java
+++ src/main/java/com/ameec/camino/controllers/TripController.java
@@ -1,5 +1,7 @@
 package com.ameec.camino.controllers;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,7 +9,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ameec.camino.dtos.UserDto;
 import com.ameec.camino.entities.Trip;
 import com.ameec.camino.services.TripService;
 
@@ -24,6 +25,18 @@ public class TripController {
     @GetMapping("/{userId}")
     public Optional<Trip> getTripByUserId(@PathVariable Long userId) {
         return tripService.getTripByUserId(userId);
+    }
+
+    @GetMapping("/locations")
+    public String locations() {
+        try {
+            String content = new String(Files.readAllBytes(Paths.get("src/main/resources/locations.json")));
+            return content;
+        } catch (Exception e) {
+            System.out.println("File not found");
+            e.printStackTrace();
+        }
+        return "File not found";
     }
 
 }
