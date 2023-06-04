c46e8c929e6a1feaa30ef16b268221f0d294e67f
Amee <ameecova@yahoo.com>
2023-06-04 18:46:00 +0200

    Add locations endpoint

--- src/main/java/com/ameec/camino/controllers/HomeController.java
+++ src/main/java/com/ameec/camino/controllers/HomeController.java
@@ -1,15 +1,28 @@
 package com.ameec.camino.controllers;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
 public class HomeController {
     
      @GetMapping("")
      public String home() {
          return "home";
      }
+
+    @GetMapping("/locations")
+    public String locations() {
+        try {
+            String content = new String(Files.readAllBytes(Paths.get("src/main/resources/locations.json")));
+            System.out.println(content);
+            return content;
+        } catch (Exception e) {
+            System.out.println("File not found");
+            e.printStackTrace();
+        }
+        return "File not found";
+    }
 }
